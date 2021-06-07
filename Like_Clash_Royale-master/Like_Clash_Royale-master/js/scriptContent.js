239bb3674e570177752d1c695e11cea48c441c28
Christopher Silva de Sousa <61100762+christophersousa@users.noreply.github.com>
2021-06-07 15:14:15 -0300

    Delete Like_Clash_Royale-master/Like_Clash_Royale-master directory

--- Like_Clash_Royale-master/Like_Clash_Royale-master/js/scriptContent.js
+++
@@ -1,8 +0,0 @@
- (function($){
-    $(window).on("load",function(){
-        $(".scrollContent").mCustomScrollbar({
-        	theme:"rounded-dots-dark",
-        	scrollButtons:{ enable: true }
-        });
-    });
-})(jQuery);
